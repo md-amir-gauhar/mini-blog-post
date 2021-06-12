95b05e3bae881e84422fb253235fc655ec4a1021
Md Amir Gauhar <gauharamir15@gmail.com>
2021-06-12 22:54:51 +0530

    added a post component Created to post component, so that user can go through each posts

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import "./styles.css";
 import { useQuery } from "react-query";
 import { useState } from "react";
+import Post from "./Post";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function App() {
-  const { postId, setPostId } = useState(null);
+  const [postId, setPostId] = useState(null);
+
   const { isLoading, data: posts } = useQuery("posts", () =>
     fetcher("https://jsonplaceholder.typicode.com/posts")
   );
 
   if (isLoading) return <p>Loading...</p>;
+
+  if (postId !== null) {
+    return <Post postId={postId} goBack={() => setPostId(null)} />;
+  }
 
   return (
     <div className="App">
